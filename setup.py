197d10c1c9671fd45fe3467c41d50cc75277e59d
Kemal Maulana <kemal@kkurniawan.com>
2017-09-26 11:38:46 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(name='pytorch-rnng',
+      version='0.0.0',
+      author='Kemal Kurniawan',
+      author_email='kemal@kkurniawan.com',
+      license='MIT',
+      packages=find_packages())
