a4f55e4e79592bed5105b789d001b5bd436ae453
Kemal Maulana <kemal@kkurniawan.com>
2017-09-26 21:14:27 +0700

    Reorganize source files

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 
-setup(name='pytorch-rnng',
+setup(name='rnng',
       version='0.0.0',
       author='Kemal Kurniawan',
       author_email='kemal@kkurniawan.com',
       license='MIT',
-      packages=find_packages())
+      packages=find_packages('src'),
+      package_dir={'': 'src'})
