802bd27df9d95eee55e625bebce2c8da5c1ae792
Kemal Maulana <kemal@kkurniawan.com>
2017-09-27 11:20:30 +0700

    Enable parsing/reading oracle from string

--- src/rnng/oracle.py
+++ src/rnng/oracle.py
@@ -13,6 +13,13 @@ class ShiftAction:
     def __repr__(self) -> str:
         return str(self)
 
+    @classmethod
+    def from_string(cls, line: str) -> 'ShiftAction':
+        if line != 'SHIFT':
+            raise ValueError('invalid string value for SHIFT action')
+        else:
+            return cls()
+
 
 class ReduceAction:
     def __str__(self) -> str:
@@ -20,6 +27,13 @@ class ReduceAction:
 
     def __repr__(self) -> str:
         return str(self)
+
+    @classmethod
+    def from_string(cls, line: str) -> 'ReduceAction':
+        if line != 'REDUCE':
+            raise ValueError('invalid string value for REDUCE action')
+        else:
+            return cls()
 
 
 class NTAction:
@@ -32,6 +46,14 @@ class NTAction:
     def __repr__(self) -> str:
         return str(self)
 
+    @classmethod
+    def from_string(cls, line: str) -> 'NTAction':
+        if not line.startswith('NT(') or not line.endswith(')'):
+            raise ValueError('invalid string value for NT(X) action')
+        else:
+            start = line.find('(') + 1
+            return cls(line[start:-1])
+
 
 class GenAction:
     def __init__(self, word: Word) -> None:
@@ -43,6 +65,14 @@ class GenAction:
     def __repr__(self) -> str:
         return str(self)
 
+    @classmethod
+    def from_string(cls, line: str) -> 'GenAction':
+        if not line.startswith('GEN(') or not line.endswith(')'):
+            raise ValueError('invalid string value for GEN(w) action')
+        else:
+            start = line.find('(') + 1
+            return cls(line[start:-1])
+
 
 DiscParserAction = Union[ShiftAction, ReduceAction, NTAction]
 GenParserAction = Union[GenAction, ReduceAction, NTAction]
@@ -50,6 +80,10 @@ GenParserAction = Union[GenAction, ReduceAction, NTAction]
 
 class BaseOracle:
     __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __str__(self) -> str:
+        pass
 
     @classmethod
     def get_children_spans(cls, line: str) -> Iterable[Tuple[int, int]]:
@@ -72,6 +106,10 @@ class BaseOracle:
     def from_bracketed_string(cls, line: str):
         pass
 
+    @abc.abstractclassmethod
+    def from_string(cls, line: str):
+        pass
+
 
 class DiscOracle(BaseOracle):
     def __init__(self, actions: List[DiscParserAction], pos_tags: List[NTLabel],
@@ -85,6 +123,11 @@ class DiscOracle(BaseOracle):
         self.actions = actions
         self.words = words
         self.pos_tags = pos_tags
+
+    def __str__(self) -> str:
+        out = [' '.join(self.words), ' '.join(self.pos_tags)]
+        out.extend([str(a) for a in self.actions])
+        return '\n'.join(out)
 
     @classmethod
     def from_bracketed_string(cls, line: str) -> 'DiscOracle':
@@ -110,6 +153,29 @@ class DiscOracle(BaseOracle):
             pos_tag, word = line[1:-1].split()
             return cls([ShiftAction()], [pos_tag], [word])
 
+    @classmethod
+    def from_string(cls, line: str) -> 'DiscOracle':
+        rows = line.split('\n')
+        if len(rows) < 3:
+            raise ValueError('string must have at least 3 lines (words, POS tags, actions)')
+
+        words = rows[0].strip().split()
+        pos_tags = rows[1].strip().split()
+        actions = [cls.get_disc_action_from_string(a_str) for a_str in rows[2:]]
+        return cls(actions, pos_tags, words)
+
+    @staticmethod
+    def get_disc_action_from_string(line: str) -> DiscParserAction:
+        classes = [NTAction, ShiftAction, ReduceAction]
+        for cls in classes:
+            try:
+                return cls.from_string(line)  # noqa
+            except ValueError:
+                continue
+        else:
+            raise ValueError(
+                f"'{line}' is not a valid string for any discriminative parser action")
+
 
 class GenOracle(BaseOracle):
     def __init__(self, actions: List[GenParserAction], pos_tags: List[NTLabel]) -> None:
@@ -119,6 +185,15 @@ class GenOracle(BaseOracle):
 
         self.actions = actions
         self.pos_tags = pos_tags
+
+    def __str__(self) -> str:
+        out = [' '.join(self.pos_tags)]
+        out.extend([str(a) for a in self.actions])
+        return '\n'.join(out)
+
+    @property
+    def words(self) -> List[Word]:
+        return [a.word for a in self.actions if isinstance(a, GenAction)]
 
     @classmethod
     def from_bracketed_string(cls, line: str) -> 'GenOracle':
@@ -143,6 +218,24 @@ class GenOracle(BaseOracle):
             pos_tag, word = line[1:-1].split()
             return cls([GenAction(word)], [pos_tag])
 
-    @property
-    def words(self) -> List[Word]:
-        return [a.word for a in self.actions if isinstance(a, GenAction)]
+    @classmethod
+    def from_string(cls, line: str) -> 'GenOracle':
+        rows = line.split('\n')
+        if len(rows) < 2:
+            raise ValueError('string must have at least 2 lines (POS tags, actions)')
+
+        pos_tags = rows[0].strip().split()
+        actions = [cls.get_gen_action_from_string(a_str) for a_str in rows[1:]]
+        return cls(actions, pos_tags)
+
+    @staticmethod
+    def get_gen_action_from_string(line: str) -> GenParserAction:
+        classes = [NTAction, GenAction, ReduceAction]
+        for cls in classes:
+            try:
+                return cls.from_string(line)  # noqa
+            except ValueError:
+                continue
+        else:
+            raise ValueError(
+                f"'{line}' is not a valid string for any generative parser action")
