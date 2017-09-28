53a063250593916d94d39d355f586572854d443d
Kemal Maulana <kemal@kkurniawan.com>
2017-09-28 17:04:54 +0700

    Implement StackedLSTMCell

--- src/rnng/models.py
+++ src/rnng/models.py
@@ -1,4 +1,55 @@
+from typing import Tuple
+
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.nn import Module, LSTMCell
+
+
+class StackedLSTMCell(Module):
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int = 1,
+                 dropout: float = 0.) -> None:
+        if num_layers < 1:
+            raise ValueError('number of layers is at least 1')
+
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self._cells = [LSTMCell(input_size, hidden_size) for _ in range(num_layers)]
+
+    def forward(self, inputs: Variable, init_states: Tuple[Variable, Variable]) -> Tuple[
+            Variable, Variable]:
+        # inputs: batch_size x input_size
+        # init_states: (h0, c0)
+        # h0: num_layers x batch_size x hidden_size
+        # c0: num_layers x batch_size x hidden_size
+        # outputs: (h1, c1)
+        # h1: num_layers x batch_size x hidden_size
+        # c1: num_layers x batch_size x hidden_size
+
+        assert len(self._cells) >= 1
+
+        h0, c0 = init_states
+        h1, c1 = [], []
+
+        if h0.dim() != 3 or c0.dim() != 3:
+            raise ValueError('h0 and c0 should have dimension of 3')
+        if h0.size()[0] != self.num_layers or c0.size()[0] != self.num_layers:
+            raise ValueError('first dimension of h0 and c0 should match the number of layers')
+
+        inputs = F.dropout(inputs, p=self.dropout, training=self.training)
+        next_h, next_c = self._cells[0](inputs, (h0[0], c0[0]))
+        next_h = F.dropout(next_h, p=self.dropout, training=self.training)
+        h1.append(next_h)
+        c1.append(next_c)
+        for cell, h0_layer, c0_layer in zip(self._cells[1:], h0[1:], c0[1:]):
+            next_h, next_c = cell(next_h, (h0_layer, c0_layer))
+            next_h = F.dropout(next_h, p=self.dropout, training=self.training)
+            h1.append(next_h)
+            c1.append(next_c)
+        return torch.stack(h1), torch.stack(c1)
 
 
 class EmptyStackError(Exception):
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,17 +1,63 @@
 import pytest
+import torch
+from torch.autograd import Variable
 
-from rnng.models import StackLSTM, EmptyStackError
+from rnng.models import StackedLSTMCell, StackLSTM, EmptyStackError
 
 
 class MockLSTMCell:
-    def __init__(self):
+    def __init__(self, retval=None):
+        if retval is None:
+            retval = [('a', 1), ('b', 2), ('c', 3)]
+
         self.index = 0
-        self.retval = [('a', 1), ('b', 2), ('c', 3)]
+        self.retval = retval
 
     def __call__(self, *args, **kwargs):
         res = self.retval[self.index]
         self.index = (self.index + 1) % len(self.retval)
         return res
+
+
+class TestStackedLSTMCell:
+    def test_call(self, mocker):
+        batch_size = 3
+        input_size = 10
+        hidden_size = 5
+        num_layers = 3
+        cells = [MockLSTMCell(retval=[(Variable(torch.randn(batch_size, hidden_size)),
+                                       Variable(torch.randn(batch_size, hidden_size)))
+                                      for _ in range(3)])
+                 for i in range(num_layers)]
+        mocker.patch('rnng.models.LSTMCell', side_effect=cells)
+
+        inputs = Variable(torch.randn(batch_size, input_size))
+        h0 = Variable(torch.randn(num_layers, batch_size, hidden_size))
+        c0 = Variable(torch.randn(num_layers, batch_size, hidden_size))
+        expected_h1 = torch.stack([c.retval[0][0] for c in cells])
+        expected_c1 = torch.stack([c.retval[0][1] for c in cells])
+
+        stacked_cell = StackedLSTMCell(input_size, hidden_size, num_layers=num_layers)
+        h1, c1 = stacked_cell(inputs, (h0, c0))
+
+        assert torch.equal(h1.data, expected_h1.data)
+        assert torch.equal(c1.data, expected_c1.data)
+
+    def test_num_layers_too_low(self):
+        with pytest.raises(ValueError):
+            StackedLSTMCell(10, 5, num_layers=0)
+
+    def test_init_states_fewer_than_layers(self):
+        batch_size = 4
+        input_size = 10
+        hidden_size = 5
+        stacked_cell = StackedLSTMCell(input_size, hidden_size, num_layers=3)
+        inputs = Variable(torch.randn(batch_size, input_size))
+        h0 = Variable(torch.randn(2, batch_size, hidden_size))
+        c0 = Variable(torch.randn(2, batch_size, hidden_size))
+
+        with pytest.raises(ValueError):
+            stacked_cell(inputs, (h0, c0))
 
 
 class TestStackLSTM:
