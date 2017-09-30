5eb89e90f3cf3ed41ac6ed323efc45c296be5f77
Kemal Maulana <kemal@kkurniawan.com>
2017-09-30 19:21:19 +0700

    Add tests for utils module

---
+++ tests/test_utils.py
@@ -0,0 +1,45 @@
+from rnng.utils import TermCollection
+
+
+class TestTermCollection:
+    def test_contains(self):
+        tc = TermCollection()
+        for c in list('aabbcc'):
+            tc.add(c)
+
+        assert 'a' in tc
+        assert 'b' in tc
+        assert 'c' in tc
+        assert 'd' not in tc
+
+    def test_iter(self):
+        tc = TermCollection()
+        for c in list('aabbcc'):
+            tc.add(c)
+
+        assert sorted(list('abc')) == sorted(tc)
+
+    def test_len(self):
+        tc = TermCollection()
+        for c in list('aabbcc'):
+            tc.add(c)
+
+        assert len(tc) == 3
+
+    def test_unique_ids(self):
+        tc = TermCollection()
+        for c in list('aabbcc'):
+            tc.add(c)
+
+        ids = [tc.get_id(c) for c in list('abc')]
+        assert len(ids) == len(set(ids))
+        for i in ids:
+            assert 0 <= i < len(tc)
+
+    def test_invertible(self):
+        tc = TermCollection()
+        for c in list('aabbcc'):
+            tc.add(c)
+
+        for c in list('abc'):
+            assert tc.get_term(tc.get_id(c)) == c
