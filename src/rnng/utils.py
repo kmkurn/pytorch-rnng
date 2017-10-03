217fb1544368c3c60f48912072a5f4acb19a0ef2
Kemal Maulana <kemal@kkurniawan.com>
2017-10-03 10:56:58 +0700

    Implement a parse tree mapper from ids to actual strings

--- src/rnng/utils.py
+++ src/rnng/utils.py
@@ -1,4 +1,7 @@
-from typing import Collection, Iterator
+from typing import Collection, Iterator, Mapping, Union
+
+from nltk.tree import Tree
+from rnng.typing import NTId, NTLabel, Word, WordId
 
 
 class TermStore(Collection[str]):
@@ -27,3 +30,29 @@ class TermStore(Collection[str]):
 
     def get_term(self, ix: int) -> str:
         return self._id2term[ix]
+
+
+class ParseTreeMapper:
+    def __init__(self, word2id: Mapping[Word, WordId], nt2id: Mapping[NTLabel, NTId]) -> None:
+        words, word_ids = tuple(zip(*word2id.items()))
+        nt_labels, nt_ids = tuple(zip(*nt2id.items()))
+
+        if len(set(word_ids)) != len(word_ids):
+            raise ValueError('word IDs should be unique')
+        if len(set(nt_ids)) != len(nt_ids):
+            raise ValueError('nonterminal IDs should be unique')
+
+        self._word2id = word2id
+        self._nt2id = nt2id
+        self._id2word = dict(zip(word_ids, words))
+        self._id2nt = dict(zip(nt_ids, nt_labels))
+
+    def __call__(self, parse_tree: Tree) -> Tree:
+        return self._map(parse_tree)
+
+    def _map(self, root: Union[Tree, WordId]) -> Tree:
+        if isinstance(root, WordId):
+            return self._id2word[root]
+        else:
+            children = [self._map(child) for child in root]
+            return Tree(self._id2nt[root.label()], children)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from rnng.utils import TermStore
+from nltk.tree import Tree
+
+from rnng.utils import TermStore, ParseTreeMapper
 
 
 class TestTermStore:
@@ -43,3 +45,28 @@ class TestTermStore:
 
         for c in list('abc'):
             assert tc.get_term(tc.get_id(c)) == c
+
+
+class TestParseTreeMapper:
+    word2id = {'John': 0, 'loves': 1, 'Mary': 2}
+    nt2id = {'S': 0, 'NP': 1, 'VP': 2}
+
+    def test_call(self):
+        parse_tree = Tree(self.nt2id['S'], [
+            Tree(self.nt2id['NP'], [
+                self.word2id['John']]),
+            Tree(self.nt2id['VP'], [
+                self.word2id['loves'],
+                Tree(self.nt2id['NP'], [
+                    self.word2id['Mary']
+                ])
+            ])
+        ])
+        exp_parse_tree = Tree('S', [
+            Tree('NP', ['John']),
+            Tree('VP', ['loves', Tree('NP', ['Mary'])])
+        ])
+
+        mapper = ParseTreeMapper(self.word2id, self.nt2id)
+
+        assert str(mapper(parse_tree)) == str(exp_parse_tree)
