8acd0d3332561995a8021bb18e6d6afa036a4b4d
Kemal Maulana <kemal@kkurniawan.com>
2017-10-05 14:55:35 +0700

    Fix decoding module to accomodate API change

--- src/rnng/decoding.py
+++ src/rnng/decoding.py
@@ -2,15 +2,28 @@ from typing import List, Tuple
 
 from torch.autograd import Variable
 
-from rnng.models import RNNGrammar
+from rnng.models import DiscRNNGrammar
 from rnng.typing import ActionId
 
 
-def greedy_decode(parser: RNNGrammar) -> List[Tuple[ActionId, Variable]]:
+def greedy_decode(parser: DiscRNNGrammar) -> List[Tuple[ActionId, Variable]]:
+    action2nt = {a: n for n, a in parser.nt2action.items()}
     result = []
     while not parser.finished:
         log_probs = parser()
         best_logprob, best_action = log_probs.data.max(0)
-        result.append((best_action[0], best_logprob[0]))
-        parser.do_action(best_action[0])
+        best_action = best_action[0]
+        best_logprob = best_logprob[0]
+        result.append((best_action, best_logprob))
+        if best_action == parser.shift_action:
+            parser.shift()
+        elif best_action == parser.reduce_action:
+            parser.reduce()
+        else:
+            try:
+                parser.push_nt(action2nt[best_action])
+            except KeyError:
+                raise KeyError(
+                    f'action ID should be of an NT(X) action but '
+                    "cannot be found in the parser's mapping: {best_action}")
     return result
--- tests/test_decoding.py
+++ tests/test_decoding.py
@@ -10,10 +10,9 @@ word2id = {'John': 0, 'loves': 1, 'Mary': 2}
 pos2id = {'NNP': 0, 'VBZ': 1}
 nt2id = {'S': 0, 'NP': 1, 'VP': 2}
 action2id = {'NT(S)': 0, 'NT(NP)': 1, 'NT(VP)': 2, 'SHIFT': 3, 'REDUCE': 4}
-action2nt = {0: 0, 1: 1, 2: 2}
+nt2action = {0: 0, 1: 1, 2: 2}
 
 
-@pytest.mark.skip(reason='RNNGrammar API changed')
 def test_greedy_decode(mocker):
     words = [word2id[w] for w in ['John', 'loves', 'Mary']]
     pos_tags = [pos2id[p] for p in ['NNP', 'VBZ', 'NNP']]
@@ -35,7 +34,7 @@ def test_greedy_decode(mocker):
                 for a in correct_actions]
     parser = DiscRNNGrammar(
         len(word2id), len(pos2id), len(nt2id), len(action2id),
-        action2id['SHIFT'], action2nt)
+        action2id['SHIFT'], nt2action)
     parser.start(list(zip(words, pos_tags)))
     mocker.patch.object(parser, 'forward', side_effect=retvals)
 
@@ -44,3 +43,33 @@ def test_greedy_decode(mocker):
     assert len(result) == len(correct_actions)
     picked_actions, log_probs = tuple(zip(*result))
     assert list(picked_actions) == correct_actions
+
+
+def test_greedy_decode_missing_action_nt(mocker):
+    words = [word2id[w] for w in ['John', 'loves', 'Mary']]
+    pos_tags = [pos2id[p] for p in ['NNP', 'VBZ', 'NNP']]
+    correct_actions = [
+        action2id['NT(S)'],
+        action2id['NT(NP)'],
+        action2id['SHIFT'],
+        action2id['REDUCE'],
+        action2id['NT(VP)'],
+        action2id['SHIFT'],
+        action2id['NT(NP)'],
+        action2id['SHIFT'],
+        action2id['REDUCE'],
+        action2id['REDUCE'],
+        action2id['REDUCE']
+    ]
+    retvals = [Variable(
+        torch.zeros(len(action2id)).scatter_(0, torch.LongTensor([a]), 1))
+                for a in correct_actions]
+    parser = DiscRNNGrammar(
+        len(word2id), len(pos2id), len(nt2id), len(action2id),
+        action2id['SHIFT'], nt2action)
+    parser.nt2action.popitem()
+    parser.start(list(zip(words, pos_tags)))
+    mocker.patch.object(parser, 'forward', side_effect=retvals)
+
+    with pytest.raises(KeyError):
+        greedy_decode(parser)
