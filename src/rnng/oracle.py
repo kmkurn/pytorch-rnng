346daf153d18339841930800392656acf06d1235
Kemal Maulana <kemal@kkurniawan.com>
2017-10-06 14:03:33 +0700

    Move actions to separate module

--- src/rnng/oracle.py
+++ src/rnng/oracle.py
@@ -5,88 +5,10 @@ from typing import Dict, Type  # noqa
 from nltk.tree import Tree
 from torch.utils.data import Dataset
 
-from rnng.typing import NTLabel, POSTag, Word
+from rnng.actions import Action, ShiftAction, ReduceAction, NTAction, GenAction
+from rnng.typing import POSTag, Word
 from rnng.typing import ActionId, NTId  # noqa
 from rnng.utils import TermStore
-
-
-class Action(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def __str__(self) -> str:
-        pass
-
-    @classmethod
-    @abc.abstractmethod
-    def from_string(cls, line: str):
-        pass
-
-
-class ShiftAction(Action):
-    def __str__(self) -> str:
-        return 'SHIFT'
-
-    def __repr__(self) -> str:
-        return str(self)
-
-    @classmethod
-    def from_string(cls, line: str) -> 'ShiftAction':
-        if line != 'SHIFT':
-            raise ValueError('invalid string value for SHIFT action')
-        else:
-            return cls()
-
-
-class ReduceAction(Action):
-    def __str__(self) -> str:
-        return 'REDUCE'
-
-    def __repr__(self) -> str:
-        return str(self)
-
-    @classmethod
-    def from_string(cls, line: str) -> 'ReduceAction':
-        if line != 'REDUCE':
-            raise ValueError('invalid string value for REDUCE action')
-        else:
-            return cls()
-
-
-class NTAction(Action):
-    def __init__(self, label: NTLabel) -> None:
-        self.label = label
-
-    def __str__(self) -> str:
-        return f'NT({self.label})'
-
-    def __repr__(self) -> str:
-        return str(self)
-
-    @classmethod
-    def from_string(cls, line: str) -> 'NTAction':
-        if not line.startswith('NT(') or not line.endswith(')'):
-            raise ValueError('invalid string value for NT(X) action')
-        else:
-            start = line.find('(') + 1
-            return cls(line[start:-1])
-
-
-class GenAction(Action):
-    def __init__(self, word: Word) -> None:
-        self.word = word
-
-    def __str__(self) -> str:
-        return f'GEN({self.word})'
-
-    def __repr__(self) -> str:
-        return str(self)
-
-    @classmethod
-    def from_string(cls, line: str) -> 'GenAction':
-        if not line.startswith('GEN(') or not line.endswith(')'):
-            raise ValueError('invalid string value for GEN(w) action')
-        else:
-            start = line.find('(') + 1
-            return cls(line[start:-1])
 
 
 class Oracle:
--- tests/test_oracle.py
+++ tests/test_oracle.py
@@ -1,67 +1,9 @@
 from nltk.tree import Tree
 import pytest
 
-from rnng.oracle import (ShiftAction, ReduceAction, NTAction, GenAction, DiscOracle,
-                         GenOracle, OracleDataset)
+from rnng.actions import ShiftAction, ReduceAction, NTAction
+from rnng.oracle import DiscOracle, GenOracle, OracleDataset
 from rnng.utils import TermStore
-
-
-class TestShiftAction:
-    def test_to_string(self):
-        a = ShiftAction()
-        assert str(a) == 'SHIFT'
-
-    def test_from_string(self):
-        a = ShiftAction.from_string('SHIFT')
-        assert isinstance(a, ShiftAction)
-
-    def test_from_invalid_string(self):
-        with pytest.raises(ValueError):
-            ShiftAction.from_string('asdf')
-
-
-class TestReduceAction:
-    def test_to_string(self):
-        a = ReduceAction()
-        assert str(a) == 'REDUCE'
-
-    def test_from_string(self):
-        a = ReduceAction.from_string('REDUCE')
-        assert isinstance(a, ReduceAction)
-
-    def test_from_invalid_string(self):
-        with pytest.raises(ValueError):
-            ReduceAction.from_string('asdf')
-
-
-class TestNTAction:
-    def test_to_string(self):
-        a = NTAction('NP')
-        assert str(a) == 'NT(NP)'
-
-    def test_from_string(self):
-        a = NTAction.from_string('NT(NP)')
-        assert isinstance(a, NTAction)
-        assert a.label == 'NP'
-
-    def test_from_invalid_string(self):
-        with pytest.raises(ValueError):
-            NTAction.from_string('asdf')
-
-
-class TestGenAction:
-    def test_to_string(self):
-        a = GenAction('asdf')
-        assert str(a) == 'GEN(asdf)'
-
-    def test_from_string(self):
-        a = GenAction.from_string('GEN(asdf)')
-        assert isinstance(a, GenAction)
-        assert a.word == 'asdf'
-
-    def test_from_invalid_string(self):
-        with pytest.raises(ValueError):
-            GenAction.from_string('asdf')
 
 
 class TestDiscOracle:
