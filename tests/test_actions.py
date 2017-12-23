29b98377da04c6da71297bb12ab933fc4090cd11
Kemal Maulana <kemal@kkurniawan.com>
2017-12-23 20:20:24 +0700

    Refactor test_actions.py to use `make_action` method

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -8,20 +8,26 @@ from rnng.actions import ShiftAction, ReduceAction, NTAction, GenAction
 class TestShiftAction:
     as_str = 'SHIFT'
 
+    @staticmethod
+    def make_action():
+        return ShiftAction()
+
     def test_eq(self):
-        assert ShiftAction() == ShiftAction()
-        assert ShiftAction() != ReduceAction()
+        a1 = self.make_action()
+        a2 = self.make_action()
+        assert a1 == a2
+        assert a1 != 'foo'
 
     def test_hash(self):
-        a = ShiftAction()
+        a = self.make_action()
         assert hash(a) == hash(self.as_str)
 
     def test_str(self):
-        a = ShiftAction()
+        a = self.make_action()
         assert str(a) == self.as_str
 
     def test_execute_on(self):
-        a = ShiftAction()
+        a = self.make_action()
         fake_parser = Mock()
         a.execute_on(fake_parser)
         fake_parser.shift.assert_called_once_with()
@@ -35,7 +41,7 @@ class TestShiftAction:
             ShiftAction.from_string('asdf')
 
     def test_verify_on(self):
-        a = ShiftAction()
+        a = self.make_action()
         fake_parser = Mock()
         a.verify_on(fake_parser)
         fake_parser.verify_shift.assert_called_once_with()
@@ -44,20 +50,26 @@ class TestShiftAction:
 class TestReduceAction:
     as_str = 'REDUCE'
 
+    @staticmethod
+    def make_action():
+        return ReduceAction()
+
     def test_eq(self):
-        assert ReduceAction() == ReduceAction()
-        assert ReduceAction() != ShiftAction()
+        a1 = self.make_action()
+        a2 = self.make_action()
+        assert a1 == a2
+        assert ReduceAction() != 'foo'
 
     def test_hash(self):
-        a = ReduceAction()
+        a = self.make_action()
         assert hash(a) == hash(self.as_str)
 
     def test_str(self):
-        a = ReduceAction()
+        a = self.make_action()
         assert str(a) == self.as_str
 
     def test_execute_on(self):
-        a = ReduceAction()
+        a = self.make_action()
         fake_parser = Mock()
         a.execute_on(fake_parser)
         fake_parser.reduce.assert_called_once_with()
@@ -71,7 +83,7 @@ class TestReduceAction:
             ReduceAction.from_string('asdf')
 
     def test_verify_on(self):
-        a = ReduceAction()
+        a = self.make_action()
         fake_parser = Mock()
         a.verify_on(fake_parser)
         fake_parser.verify_reduce.assert_called_once_with()
@@ -80,24 +92,29 @@ class TestReduceAction:
 class TestNTAction:
     as_str = 'NT({label})'
 
+    @staticmethod
+    def make_action(label='NP'):
+        return NTAction(label)
+
     def test_eq(self):
-        a = NTAction('NP')
-        assert a == NTAction(a.label)
-        assert a != NTAction('asdf')
-        assert a != ShiftAction()
+        a1 = self.make_action()
+        a2 = self.make_action()
+        a3 = self.make_action('VP')
+        assert a1 == a2
+        assert a1 != a3
+        assert a1 != 'foo'
 
     def test_hash(self):
-        label = 'NP'
-        a = NTAction(label)
-        assert hash(a) == hash(self.as_str.format(label=label))
+        a = self.make_action()
+        assert hash(a) == hash(self.as_str.format(label=a.label))
 
     def test_str(self):
         label = 'NP'
-        a = NTAction(label)
+        a = self.make_action(label=label)
         assert str(a) == self.as_str.format(label=label)
 
     def test_execute_on(self):
-        a = NTAction('NP')
+        a = self.make_action()
         fake_parser = Mock()
         a.execute_on(fake_parser)
         fake_parser.push_nt.assert_called_once_with(a.label)
@@ -113,7 +130,7 @@ class TestNTAction:
             NTAction.from_string('asdf')
 
     def test_verify_on(self):
-        a = NTAction('NP')
+        a = self.make_action()
         fake_parser = Mock()
         a.verify_on(fake_parser)
         fake_parser.verify_push_nt.assert_called_once_with()
@@ -122,20 +139,26 @@ class TestNTAction:
 class TestGenAction:
     as_str = 'GEN({word})'
 
+    @staticmethod
+    def make_action(word='asdf'):
+        return GenAction(word)
+
     def test_eq(self):
-        a = GenAction('asdf')
-        assert a == GenAction(a.word)
-        assert a != GenAction('fdsa')
-        assert a != ReduceAction()
+        a1 = self.make_action()
+        a2 = self.make_action()
+        a3 = self.make_action(word='fdsa')
+        assert a1 == a2
+        assert a1 != a3
+        assert a1 != 'foo'
 
     def test_hash(self):
         word = 'asdf'
-        a = GenAction(word)
+        a = self.make_action(word=word)
         assert hash(a) == hash(self.as_str.format(word=word))
 
     def test_str(self):
         word = 'asdf'
-        a = GenAction(word)
+        a = self.make_action(word=word)
         assert str(a) == self.as_str.format(word=word)
 
     def test_from_string(self):
