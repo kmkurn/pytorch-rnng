93c12bdf888f6cab169e87708e0c5102f5a55b27
Kemal Maulana <kemal@kkurniawan.com>
2017-12-25 20:05:25 +0700

    Create Trainer class

--- src/rnng/commands/train.py
+++ src/rnng/commands/train.py
@@ -1,25 +1,6 @@
 import argparse
-import json
-import logging
-import os
-import random
 
-from nltk.corpus.reader import BracketParseCorpusReader
-from torchtext.data import Dataset, Field
-import dill
-import torch
-import torch.optim as optim
-import torchnet as tnt
-
-from rnng.actions import Action, NTAction, ReduceAction, ShiftAction
-from rnng.example import make_example
-from rnng.iterator import SimpleIterator
-from rnng.models import DiscRNNG
-from rnng.oracle import DiscOracle
-
-
-logging.basicConfig(format='%(levelname)s - %(name)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+from rnng.trainer import Trainer
 
 
 def make_parser(subparsers=None) -> argparse.ArgumentParser:
@@ -32,6 +13,9 @@ def make_parser(subparsers=None) -> argparse.ArgumentParser:
     parser.add_argument(
         '-t', '--train-corpus', required=True, metavar='FILE', help='path to train corpus')
     parser.add_argument(
+        '-s', '--save-to', required=True, metavar='DIR',
+        help='directory to save the training artifacts')
+    parser.add_argument(
         '-d', '--dev-corpus', metavar='FILE', help='path to dev corpus')
     parser.add_argument(
         '--encoding', default='utf-8', help='file encoding to use')
@@ -42,48 +26,44 @@ def make_parser(subparsers=None) -> argparse.ArgumentParser:
         '--no-lower', action='store_false', dest='lower',
         help='whether not to lowercase the words')
     parser.add_argument(
-        '--min-freq', type=int, default=2,
+        '--min-freq', type=int, default=2, metavar='NUMBER',
         help='minimum word frequency to be included in the vocabulary')
     parser.add_argument(
-        '--word-embedding-size', type=int, default=32,
+        '--word-embedding-size', type=int, default=32, metavar='NUMBER',
         help='dimension of word embeddings (default: 32)')
     parser.add_argument(
-        '--pos-embedding-size', type=int, default=12,
+        '--pos-embedding-size', type=int, default=12, metavar='NUMBER',
         help='dimension of POS tag embeddings (default: 12)')
     parser.add_argument(
-        '--nt-embedding-size', type=int, default=60,
+        '--nt-embedding-size', type=int, default=60, metavar='NUMBER',
         help='dimension of nonterminal embeddings (default: 12)')
     parser.add_argument(
-        '--action-embedding-size', type=int, default=16,
+        '--action-embedding-size', type=int, default=16, metavar='NUMBER',
         help='dimension of action embeddings (default: 16)')
     parser.add_argument(
-        '--input-size', type=int, default=128,
+        '--input-size', type=int, default=128, metavar='NUMBER',
         help='input dimension of the LSTM parser state encoders (default: 128)')
     parser.add_argument(
-        '--hidden-size', type=int, default=128,
+        '--hidden-size', type=int, default=128, metavar='NUMBER',
         help='hidden dimension of the LSTM parser state encoders (default: 128)')
     parser.add_argument(
-        '--num-layers', type=int, default=2,
+        '--num-layers', type=int, default=2, metavar='NUMBER',
         help='number of layers of the LSTM parser state encoders and composers (default: 2)')
     parser.add_argument(
-        '--dropout', type=float, default=0.5, help='dropout rate (default: 0.5)')
+        '--dropout', type=float, default=0.5, metavar='NUMBER',
+        help='dropout rate (default: 0.5)')
     parser.add_argument(
-        '--learning-rate', type=float, default=0.001, help='learning rate (default: 0.001)')
+        '--learning-rate', type=float, default=0.001, metavar='NUMBER',
+        help='learning rate (default: 0.001)')
     parser.add_argument(
-        '--max-epochs', type=int, default=20,
+        '--max-epochs', type=int, default=20, metavar='NUMBER',
         help='maximum number of epochs to train (default: 20)')
     parser.add_argument(
-        '--patience', type=int, default=10,
-        help='number of epochs to wait before early stopping (default: 10)')
+        '--evalb', metavar='FILE', help='evalb executable file')
     parser.add_argument(
-        '-s', '--save-to', required=True, metavar='DIR',
-        help='directory to save the training artifacts')
+        '--evalb-params', metavar='FILE', help='evalb params file')
     parser.add_argument(
-        '--evalb', help='path to evalb executable')
-    parser.add_argument(
-        '--evalb-params', help='path to evalb params file')
-    parser.add_argument(
-        '--log-interval', type=int, default=10,
+        '--log-interval', type=int, default=10, metavar='NUMBER',
         help='print logs every this number of iterations (default: 10)')
     parser.add_argument(
         '--seed', type=int, default=25122017, help='random seed')
@@ -95,164 +75,9 @@ def make_parser(subparsers=None) -> argparse.ArgumentParser:
 
 
 def main(args: argparse.Namespace) -> None:
-    random.seed(args.seed)
-    torch.manual_seed(args.seed)
-
-    os.makedirs(args.save_to, exist_ok=True)
-    fields_dict_path = os.path.join(args.save_to, 'fields_dict.pkl')
-    model_metadata_path = os.path.join(args.save_to, 'model_metadata.json')
-    model_state_dict_path = os.path.join(args.save_to, 'model_state_dict.pth')
-
-    def make_dataset(corpus, fields):
-        reader = BracketParseCorpusReader(
-            *os.path.split(corpus), encoding=args.encoding, detect_blocks='sexpr')
-        oracles = [DiscOracle.from_parsed_sent(s) for s in reader.parsed_sents()]
-        examples = [make_example(x, fields) for x in oracles]
-        return Dataset(examples, fields)
-
-    ACTIONS = Field(pad_token=None, unk_token=None)
-    NONTERMS = Field(pad_token=None)
-    POS_TAGS = Field(pad_token=None)
-    WORDS = Field(pad_token=None, lower=args.lower)
-    fields = [
-        ('actions', ACTIONS), ('nonterms', NONTERMS), ('pos_tags', POS_TAGS), ('words', WORDS)]
-
-    logger.info('Reading train corpus from %s', args.train_corpus)
-    train_dataset = make_dataset(args.train_corpus, fields)
-    train_iterator = SimpleIterator(train_dataset, device=args.device)
-    dev_iterator = None
-    if args.dev_corpus is not None:
-        logger.info('Reading dev corpus from %s', args.dev_corpus)
-        dev_dataset = make_dataset(args.dev_corpus, fields)
-        dev_iterator = SimpleIterator(dev_dataset, train=False, device=args.device)
-
-    logger.info('Building vocabulary')
-    ACTIONS.build_vocab(train_dataset)
-    NONTERMS.build_vocab(train_dataset)
-    POS_TAGS.build_vocab(train_dataset)
-    WORDS.build_vocab(train_dataset, min_freq=args.min_freq)
-
-    num_words = len(WORDS.vocab)
-    num_pos = len(POS_TAGS.vocab)
-    num_nt = len(NONTERMS.vocab)
-    num_actions = len(ACTIONS.vocab)
-    logger.info(
-        'Found %d words, %d POS tags, %d nonterminals, and %d actions',
-        num_words, num_pos, num_nt, num_actions)
-
-    logger.info('Saving fields dict to %s', fields_dict_path)
-    torch.save(dict(fields), fields_dict_path, pickle_module=dill)
-
-    logger.info('Creating models')
-    action2nt = {}
-    for action_id, actionstr in enumerate(ACTIONS.vocab.itos):
-        action = Action.from_string(actionstr)
-        if isinstance(action, NTAction):
-            action2nt[action_id] = NONTERMS.vocab.stoi[action.label]
-    model_args = (
-        num_words, num_pos, num_nt, num_actions,
-        ACTIONS.vocab.stoi[str(ShiftAction())], ACTIONS.vocab.stoi[str(ReduceAction())],
-        action2nt)
-    model_kwargs = dict(
-        word_embedding_size=args.word_embedding_size,
-        pos_embedding_size=args.pos_embedding_size,
-        nt_embedding_size=args.nt_embedding_size,
-        action_embedding_size=args.action_embedding_size,
-        input_size=args.input_size,
-        hidden_size=args.hidden_size,
-        num_layers=args.num_layers,
-        dropout=args.dropout
-    )
-    model = DiscRNNG(*model_args, **model_kwargs)
-
-    logger.info('Saving model metadata to %s', model_metadata_path)
-    with open(model_metadata_path, 'w') as f:
-        json.dump({'args': model_args, 'kwargs': model_kwargs}, f, sort_keys=True, indent=2)
-
-    loss_meter = tnt.meter.AverageValueMeter()
-    speed_meter = tnt.meter.AverageValueMeter()
-    batch_timer = tnt.meter.TimeMeter(None)
-    epoch_timer = tnt.meter.TimeMeter(None)
-    train_timer = tnt.meter.TimeMeter(None)
-    engine = tnt.engine.Engine()
-    best_loss = float('inf')
-    current_patience = 0
-
-    def net(sample):
-        words = sample.words.squeeze(1)
-        pos_tags = sample.pos_tags.squeeze(1)
-        actions = sample.actions.squeeze(1)
-        llh = model(words, pos_tags, actions)
-        return -llh, None
-
-    def reset_meters():
-        loss_meter.reset()
-        speed_meter.reset()
-
-    def on_start(state):
-        if state['train']:
-            train_timer.reset()
-        else:
-            reset_meters()
-
-    def on_start_epoch(state):
-        reset_meters()
-        epoch_timer.reset()
-
-    def on_sample(state):
-        batch_timer.reset()
-
-    def on_forward(state):
-        elapsed_time = batch_timer.value()
-        loss_meter.add(state['loss'].data[0])
-        speed_meter.add(state['sample'].words.size(1) / elapsed_time)
-        if state['train'] and (state['t'] + 1) % args.log_interval == 0:
-            epoch = (state['t'] + 1) / len(state['iterator'])
-            loss, _ = loss_meter.value()
-            speed, _ = speed_meter.value()
-            logger.info(
-                'Epoch %.4f (%.4fs): %.2f samples/sec | loss %.4f',
-                epoch, elapsed_time, speed, loss)
-
-    def on_end_epoch(state):
-        epoch = state['epoch']
-        elapsed_time = epoch_timer.value()
-        loss, _ = loss_meter.value()
-        speed, _ = speed_meter.value()
-        logger.info('Epoch %d done (%.4fs): %.2f samples/sec | loss %.4f',
-                    epoch, elapsed_time, speed, loss)
-        if dev_iterator is None:
-            logger.info('Saving model parameters to %s', model_state_dict_path)
-            torch.save(model.state_dict(), model_state_dict_path)
-        else:
-            engine.test(net, dev_iterator)
-            loss, _ = loss_meter.value()
-            speed, _ = speed_meter.value()
-            logger.info('Evaluating on dev corpus: %.2f samples/sec | loss %.4f', speed, loss)
-            if loss < best_loss - 1e-6:
-                logger.info('New best model found, saving to %s', model_state_dict_path)
-                torch.save(model.state_dict(), model_state_dict_path)
-                global current_patience
-                current_patience = 0
-            else:
-                current_patience += 1
-                if current_patience >= args.patience:
-                    logger.info(
-                        'No best model found after %d epochs, stopping', current_patience)
-                    # Force training to stop
-                    state['epoch'] = state['maxepoch'] + 1
-
-    def on_end(state):
-        if state['train']:
-            elapsed_time = train_timer.value()
-            logger.info('Training done in %.4fs', elapsed_time)
-
-    engine.hooks['on_start'] = on_start
-    engine.hooks['on_start_epoch'] = on_start_epoch
-    engine.hooks['on_sample'] = on_sample
-    engine.hooks['on_forward'] = on_forward
-    engine.hooks['on_end_epoch'] = on_end_epoch
-    engine.hooks['on_end'] = on_end
-
-    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
-    engine.train(net, train_iterator, args.max_epochs, optimizer)
+    kwargs = vars(args)
+    kwargs.pop('func', None)
+    train_corpus = kwargs.pop('train_corpus')
+    save_to = kwargs.pop('save_to')
+    trainer = Trainer(train_corpus, save_to, **kwargs)
+    trainer.run()
---
+++ src/rnng/trainer.py
@@ -0,0 +1,269 @@
+import json
+import logging
+import os
+import random
+import tarfile
+
+from nltk.corpus.reader import BracketParseCorpusReader
+from torchtext.data import Dataset, Field
+import dill
+import torch
+import torch.optim as optim
+import torchnet as tnt
+
+from rnng.actions import Action, NTAction, ReduceAction, ShiftAction
+from rnng.example import make_example
+from rnng.iterator import SimpleIterator
+from rnng.models import DiscRNNG
+from rnng.oracle import DiscOracle
+
+
+class Trainer(object):
+    def __init__(self,
+                 train_corpus,
+                 save_to,
+                 dev_corpus=None,
+                 encoding='utf-8',
+                 rnng_type='discriminative',
+                 lower=True,
+                 min_freq=2,
+                 word_embedding_size=32,
+                 pos_embedding_size=12,
+                 nt_embedding_size=60,
+                 action_embedding_size=16,
+                 input_size=128,
+                 hidden_size=128,
+                 num_layers=2,
+                 dropout=0.5,
+                 learning_rate=0.001,
+                 max_epochs=20,
+                 evalb=None,
+                 evalb_params=None,
+                 device=-1,
+                 seed=25122017,
+                 log_interval=10,
+                 logger=None):
+        if logger is None:
+            logger = logging.getLogger(__name__)
+            logger.setLevel(logging.INFO)
+            handler = logging.StreamHandler()
+            handler.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(levelname)s - %(name)s - %(message)s')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
+        self.train_corpus = train_corpus
+        self.save_to = save_to
+        self.dev_corpus = dev_corpus
+        self.encoding = encoding
+        self.rnng_type = 'discriminative'
+        self.lower = lower
+        self.min_freq = min_freq
+        self.word_embedding_size = word_embedding_size
+        self.pos_embedding_size = pos_embedding_size
+        self.nt_embedding_size = nt_embedding_size
+        self.action_embedding_size = action_embedding_size
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.learning_rate = learning_rate
+        self.max_epochs = max_epochs
+        self.evalb = evalb
+        self.evalb_params = evalb_params
+        self.device = device
+        self.seed = seed
+        self.log_interval = log_interval
+        self.logger = logger
+
+        self.loss_meter = tnt.meter.AverageValueMeter()
+        self.speed_meter = tnt.meter.AverageValueMeter()
+        self.batch_timer = tnt.meter.TimeMeter(None)
+        self.epoch_timer = tnt.meter.TimeMeter(None)
+        self.train_timer = tnt.meter.TimeMeter(None)
+        self.engine = tnt.engine.Engine()
+
+    def reset_meters(self):
+        self.loss_meter.reset()
+        self.speed_meter.reset()
+
+    def set_random_seed(self):
+        self.logger.info('Setting random seed to %d', self.seed)
+        random.seed(self.seed)
+        torch.manual_seed(self.seed)
+
+    def prepare_for_serialization(self):
+        self.logger.info('Preparing serialization directory in %s', self.save_to)
+        os.makedirs(self.save_to, exist_ok=True)
+        self.fields_dict_path = os.path.join(self.save_to, 'fields_dict.pkl')
+        self.model_metadata_path = os.path.join(self.save_to, 'model_metadata.json')
+        self.model_params_path = os.path.join(self.save_to, 'model_params.pth')
+        self.artifacts_path = os.path.join(self.save_to, 'artifacts.tar.gz')
+
+    def init_fields(self):
+        self.ACTIONS = Field(pad_token=None, unk_token=None)
+        self.NONTERMS = Field(pad_token=None)
+        self.POS_TAGS = Field(pad_token=None)
+        self.WORDS = Field(pad_token=None, lower=self.lower)
+        self.fields = [
+            ('actions', self.ACTIONS), ('nonterms', self.NONTERMS),
+            ('pos_tags', self.POS_TAGS), ('words', self.WORDS),
+        ]
+
+    def process_corpora(self):
+        self.logger.info('Reading train corpus from %s', self.train_corpus)
+        self.train_dataset = self.make_dataset(self.train_corpus)
+        self.train_iterator = SimpleIterator(self.train_dataset, device=self.device)
+        self.dev_dataset = None
+        self.dev_iterator = None
+        if self.dev_corpus is not None:
+            self.logger.info('Reading dev corpus from %s', self.dev_corpus)
+            self.dev_dataset = self.make_dataset(self.dev_corpus)
+            self.dev_iterator = SimpleIterator(
+                self.dev_dataset, train=False, device=self.device)
+
+    def build_vocabularies(self):
+        self.logger.info('Building vocabularies')
+        self.ACTIONS.build_vocab(self.train_dataset)
+        self.NONTERMS.build_vocab(self.train_dataset)
+        self.POS_TAGS.build_vocab(self.train_dataset)
+        self.WORDS.build_vocab(self.train_dataset, min_freq=self.min_freq)
+
+        self.num_words = len(self.WORDS.vocab)
+        self.num_pos = len(self.POS_TAGS.vocab)
+        self.num_nt = len(self.NONTERMS.vocab)
+        self.num_actions = len(self.ACTIONS.vocab)
+        self.logger.info(
+            'Found %d words, %d POS tags, %d nonterminals, and %d actions',
+            self.num_words, self.num_pos, self.num_nt, self.num_actions)
+
+        self.logger.info('Saving fields dict to %s', self.fields_dict_path)
+        torch.save(dict(self.fields), self.fields_dict_path, pickle_module=dill)
+
+    def build_model(self):
+        self.logger.info('Building model')
+        action2nt = {}
+        for action_id, actionstr in enumerate(self.ACTIONS.vocab.itos):
+            action = Action.from_string(actionstr)
+            if isinstance(action, NTAction):
+                action2nt[action_id] = self.NONTERMS.vocab.stoi[action.label]
+        model_args = (
+            self.num_words, self.num_pos, self.num_nt, self.num_actions,
+            self.ACTIONS.vocab.stoi[str(ShiftAction())],
+            self.ACTIONS.vocab.stoi[str(ReduceAction())],
+            action2nt)
+        model_kwargs = dict(
+            word_embedding_size=self.word_embedding_size,
+            pos_embedding_size=self.pos_embedding_size,
+            nt_embedding_size=self.nt_embedding_size,
+            action_embedding_size=self.action_embedding_size,
+            input_size=self.input_size,
+            hidden_size=self.hidden_size,
+            num_layers=self.num_layers,
+            dropout=self.dropout
+        )
+        self.model = DiscRNNG(*model_args, **model_kwargs)
+        if self.device >= 0:
+            self.model.cuda(self.device)
+
+        self.logger.info('Saving model metadata to %s', self.model_metadata_path)
+        with open(self.model_metadata_path, 'w') as f:
+            json.dump({'args': model_args, 'kwargs': model_kwargs}, f, sort_keys=True, indent=2)
+        self.logger.info('Saving initial model parameters to %s', self.model_params_path)
+        torch.save(self.model.state_dict(), self.model_params_path)
+
+    def build_optimizer(self):
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+
+    def save_artifacts(self):
+        self.logger.info('Saving training artifacts to %s', self.artifacts_path)
+        with tarfile.open(self.artifacts_path, 'w:gz') as f:
+            artifact_names = 'fields_dict model_metadata model_params'.split()
+            for name in artifact_names:
+                path = getattr(self, f'{name}_path')
+                f.add(path, arcname=os.path.basename(path))
+
+    def run(self):
+        self.set_random_seed()
+        self.prepare_for_serialization()
+        self.init_fields()
+        self.process_corpora()
+        self.build_vocabularies()
+        self.build_model()
+        self.build_optimizer()
+
+        self.engine.hooks['on_start'] = self.on_start
+        self.engine.hooks['on_start_epoch'] = self.on_start_epoch
+        self.engine.hooks['on_sample'] = self.on_sample
+        self.engine.hooks['on_forward'] = self.on_forward
+        self.engine.hooks['on_end_epoch'] = self.on_end_epoch
+        self.engine.hooks['on_end'] = self.on_end
+
+        try:
+            self.engine.train(
+                self.network, self.train_iterator, self.max_epochs, self.optimizer)
+        except KeyboardInterrupt:
+            self.logger.info('Training interrupted, aborting')
+            self.save_artifacts()
+
+    def network(self, sample):
+        words = sample.words.squeeze(1)
+        pos_tags = sample.pos_tags.squeeze(1)
+        actions = sample.actions.squeeze(1)
+        llh = self.model(words, pos_tags, actions)
+        return -llh, None
+
+    def on_start(self, state):
+        if state['train']:
+            self.train_timer.reset()
+        else:
+            self.reset_meters()
+
+    def on_start_epoch(self, state):
+        self.reset_meters()
+        self.epoch_timer.reset()
+
+    def on_sample(self, state):
+        self.batch_timer.reset()
+
+    def on_forward(self, state):
+        elapsed_time = self.batch_timer.value()
+        self.loss_meter.add(state['loss'].data[0])
+        self.speed_meter.add(state['sample'].words.size(1) / elapsed_time)
+        if state['train'] and (state['t'] + 1) % self.log_interval == 0:
+            epoch = (state['t'] + 1) / len(state['iterator'])
+            loss, _ = self.loss_meter.value()
+            speed, _ = self.speed_meter.value()
+            self.logger.info(
+                'Epoch %.4f (%.4fs): %.2f samples/sec | loss %.4f',
+                epoch, elapsed_time, speed, loss)
+
+    def on_end_epoch(self, state):
+        epoch = state['epoch']
+        elapsed_time = self.epoch_timer.value()
+        loss, _ = self.loss_meter.value()
+        speed, _ = self.speed_meter.value()
+        self.logger.info('Epoch %d done (%.4fs): %.2f samples/sec | loss %.4f',
+                         epoch, elapsed_time, speed, loss)
+        if self.dev_iterator is None:
+            self.logger.info('Saving model parameters to %s', self.model_params_path)
+            torch.save(self.model.state_dict(), self.model_params_path)
+        else:
+            self.engine.test(self.network, self.dev_iterator)
+            loss, _ = self.loss_meter.value()
+            speed, _ = self.speed_meter.value()
+            self.logger.info(
+                'Evaluating on dev corpus: %.2f samples/sec | loss %.4f', speed, loss)
+
+    def on_end(self, state):
+        if state['train']:
+            elapsed_time = self.train_timer.value()
+            self.logger.info('Training done in %.4fs', elapsed_time)
+            self.save_artifacts()
+
+    def make_dataset(self, corpus):
+        reader = BracketParseCorpusReader(
+            *os.path.split(corpus), encoding=self.encoding, detect_blocks='sexpr')
+        oracles = [DiscOracle.from_parsed_sent(s) for s in reader.parsed_sents()]
+        examples = [make_example(x, self.fields) for x in oracles]
+        return Dataset(examples, self.fields)
